cd95aa9777a76b168983c1581b4268d32820af31
neobonde <201404519@post.au.dk>
2017-04-13 16:43:25 +0200

    Added an easy way to change content of the webpage

---
+++ content.json.js
@@ -0,0 +1,254 @@
+var jsonData = {
+    "navbar": {
+        "logo": "img/logo.png",
+        "menu1": "Menu'er",
+        "menu2": "Buffet Menu'er",
+        "menu3": "Selvskab Lokaler",
+        "menu4": "Udlejning",
+        "menu5": "Kontakt",
+    },
+    "title": {
+        "header1": "Velkommen til </br>",
+        "header2": "Levering af mad weekender og aften",
+        "button": "Se Menu'en",
+        "logo": "img/logo.png",
+    },
+    // Almindelige menu'er her ----
+    "menu1": {
+        "title": "Menu'er",
+        "flavorText": "Tag et kig på alle vores lækre retter og menu'er </br></br> Klik på en kategori for at få se hvad vi tilbyder",
+        "columns": "4",
+        "noLink" : "false",
+        "modals": [
+            // Kopier her fra
+            {
+                "title": "Forretter",
+                "price": "35kr. pr. person",
+                "extraText": "",
+                "image": "img/portfolio/forretter.jpg",
+                "items": [
+                    "Tunmousse på salatbund med flutes og dressing",
+                    "Tarteletter med hønsekødsfyld",
+                    "Laksetatar på salatbund med ristet rugbrød",
+                    "Varmrøget laks med safran creme",
+                ],
+            },
+            //Og Her til for at indsætte et nyt menu punkt
+            {
+                "title": "Desserter",
+                "price": "30kr. pr. person",
+                "extraText": "",
+                "image": "img/portfolio/dessert.jpg",
+                "items": [
+                    "Rabarber trifili",
+                    "Chokolademousse",
+                    "Citronfromage",
+                    "Brownies (20kr. pr. person)",
+                ],
+            },
+            {
+                "title": "Bonde's favorit",
+                "price": "80kr. pr. person",
+                "extraText": "Minimum bestilling 10 personer",
+                "image": "img/portfolio/favorit.jpg",
+                "items": [
+                    "Hjemmelavet pulled pork (marineret nakkefilet) stegt i 10 timer",
+                    "Hjemmelavet Coleslaw",
+                    "Hjemmelavet BBQ sauce",
+                    "Store grove burgerboller",
+                ],
+            },
+            {
+                "title": "Tapas",
+                "price": "75kr. pr. person",
+                "extraText": "Som hovedret min 15 personer </br> Som forret til buffet 45kr. pr. person",
+                "image": "img/portfolio/tapas.jpg",
+                "items": [
+                    "3 slags spansk pølse",
+                    "Spegeskinke",
+                    "2 slags pesto",
+                    "Hummus",
+                    "Oliven",
+                    "Oliventappanade",
+                    "Saltmandler",
+                    "Vælg mellem 3 slags langtidshævet kuglebrød (malt krydder eller naturel)",
+                ],
+            },
+            {
+                "title": "Grillstegning",
+                "price": "Pris 295 kr pr time ",
+                "extraText": "Jeg tager gerne ud og griller til jeres firmafest, fødselsdage mv. i Odder kommune. </br></br> Jeg griller bla:",
+                "image": "img/portfolio/grill.jpg",
+                "items": [
+                    "Hele skinker",
+                    "Oksefilet",
+                    "Oksemørbrad",
+                    "Krydret porchettastege",
+                ],
+            },
+            {
+                "title": "Grill tapas",
+                "price": " - ",
+                "extraText": "Grill tapas er små stykker kød Så alle kan smage alle slags",
+                "image": "img/portfolio/grillTapas.jpg",
+                "items": [
+                    "Grill kød",
+                    "Bøffer alm/marineret",
+                    "Karrymarineret kylling",
+                    "Frankfurter pølsere",
+                    "BBQ ben",
+                    "Zouvaliki spyd",
+                ],
+            },
+            {
+                "title": "Klassearrangement-polterabend",
+                "price": "45kr. pr. person ",
+                "extraText": "Alle retter kan bestilles med salat og flutes </br> Salat 10kr. </br> Flutes 5kr.",
+                "image": "img/portfolio/polterabend.jpg",
+                "items": [
+                    "Helstegt pattegris 30-65 kg",
+                    "Flødekartofler eller bagt kartoffel",
+                    "Blandet salat",
+                    "Hjemmebagt Møllehjul",
+                ],
+            },
+            {
+                "title": "Helstegt Pattegris",
+                "price": "160kr. pr. person ",
+                "extraText": "Minimum bestilling 30 personer </br> Bemærk jeg griller og udskære hos jer",
+                "image": "img/portfolio/gris.png",
+                "items": [
+                    "Helstegt pattegris 30-65 kg",
+                    "Flødekartofler eller bagt kartoffel",
+                    "Blandet salat",
+                    "Hjemmebagt Møllehjul",
+                ],
+            },
+        ]
+    },
+
+    // Buffet menu'er her ---------------
+
+    "menu2": {
+        "title": "Buffet Menu'er",
+        "flavorText": "Tag et kig på alle vores lækre buffet'er </br></br> Sammensæt selv din menu af retter fra de forskellige kategorier </br></br> <b>150kr.</b> pr. person",
+        "columns": "4",
+        "noLink" : "false",
+        "modals": [{
+                "title": "Kød",
+                "price": "Vælg 1-2 slags kød",
+                "extraText": "",
+                "image": "img/portfolio/kod.jpg",
+                "items": [
+                    "Rosa stegt oksefilet",
+                    "Rosa stegt kalvefilet",
+                    "Kyllingefilet med bacon",
+                    "Chili/vanilie marineret skinkesteg",
+                    "Glaseret Skinke",
+                    "Ungkvæg Culotte",
+                    "Helstegt Oksemørbrad (tillæg på 20 kr pr person)",
+                    "Langtidsstegt marineret Nakkefilet (ala Pulled Pork)",
+                    "Kold hvidvinsdampet Laks (tillæg 10 kr pr person)",
+                    "Grill pakke (grill selv)", [
+                        "Grill kød",
+                        "Bøffer alm/marineret",
+                        "Karrymarineret kylling",
+                        "Frankfurter pølser",
+                        "BBQ ben",
+                        "Zouvaliki spyd",
+                    ],
+                ],
+            },
+            {
+                "title": "Kartofler",
+                "price": "Vælg 1-2 slags kartofler",
+                "extraText": "",
+                "image": "img/portfolio/kartofler.jpg",
+                "items": [
+                    "Flødekartofler",
+                    "Kartffelsalat med Creme fraiche",
+                    "Bagekartofler med hjemmerørt hvidløgssmør",
+                    "Sommer kartoffelsalat med radiser forårsløg, ærter og friske asparges",
+                    "Små nye marinerede kartofler",
+                    "Kartoffelrøsti",
+                ],
+            },
+            {
+                "title": "Salat",
+                "price": "Vælg 1-2 slags salater",
+                "extraText": "",
+                "image": "img/portfolio/salat.jpg",
+                "items": [
+                    "Coleslaw Salat",
+                    "Salat med friske frugter",
+                    "Feta salat med agurk tomat pinjekerner mm.",
+                    "Spidskålsalat med æbler og mandler",
+                    "Tomatsalat med frisk Mozzarella",
+                    "Honningmarineret rødbedesalat m/feta",
+                    "Vandmelonsalat m/ feta (kun i sommertiden)",
+                ],
+            },
+            {
+                "title": "Brød",
+                "price": "Vælg 1 slags brød",
+                "extraText": " Med brød kan man frit vælge imellem: </br> Hummus pesto advocadocreme eller fetacreme",
+                "image": "img/portfolio/brod.jpg",
+                "items": [
+                    "talianske Flutes",
+                    "Møllehjul",
+                    "Langtidshævet kuglebrød \"malt/krydder/neutral\"",
+                ],
+            },
+        ]
+    },
+    // Selvskab lokale ----
+    "menu3": {
+        "title": "Bonde`s Mads Selskabslokale",
+        "flavorText": "Har plads til 80 personer </br> Lejes med bord stole og service. </br> Rengøring kan tilkøbes </br> <b>Pris 2500 kr</b>",
+        "images" : [
+            "img/carousel/lokale1.jpg",
+            "img/carousel/lokale2.jpg",
+            "img/carousel/lokale3.jpg",
+            "img/carousel/lokale4.jpg",
+        ]
+    },
+    // Udlejning -------
+    "menu4": {
+        "title": "Udlejning",
+        "flavorText": "Vi kan hos Bonde's Mad tilbyde udlejning af flere forskellige emner",
+        "columns": "4",
+        "noLink" : "true",
+        "modals": [
+            {
+                "title": "Partytelt",
+                "price": "2000 kr.",
+                "extraText": "6x8 meter i pvc </br> Pris ved afhentning Skovdalsvej 4",
+                "image": "img/portfolio/telt.jpg",
+            },
+            {
+                "title": "Pattegris Grill",
+                "price": "<b>500 kr.</b> pr. dag </p>",
+                "extraText": "Grill til stegning af grise op til 60 kg",
+                "image": "img/portfolio/stor_grill.jpg",
+            },
+            {
+                "title": "Kvalitets stol",
+                "price": "12 kr.</b> pr. stk. 0-40 stk. </br><b>10 kr.</b> pr. stk. 40-80 stk.",
+                "extraText": "",
+                "image": "img/portfolio/stol.jpg",
+            },
+            {
+                "title": "Borde",
+                "price": "<b>50 kr.</b> pr. stk.",
+                "extraText": " ",
+                "image": "img/portfolio/borde.jpg",
+            },
+        ]
+    },
+    "menu5": {
+        "title": "Kontakt Os",
+        "text1": "Tøv ikke med at kontakte mig! </br> Ring, mail eller besøg mig:",
+        "text2": "Bonde's Mad:</br> <a href=\"https://goo.gl/maps/8bNwwiAQkM22\">Skovdalsvej 4 </br> 8300 Odder</h3>",
+        "text3": "Tlf: <a href=\"tel:+4542835326\">42 83 53 26</a></br>E-Mail: <a href=\"mailto:Bonde@bondes-mad.dk\">Bonde@bondes-mad.dk</a>",
+    }
+};
---
+++ js/loadText.js
@@ -0,0 +1,214 @@
+main();
+
+function main() {
+    var editables = document.getElementsByClassName("editable")
+
+    // console.log(editables);
+
+    for (var i = 0; i < editables.length; i++) {
+        // console.log(getId(editables[i].id));
+        var sectionId = getSection(editables[i].id);
+        var id = getId(editables[i].id);
+        if (id == "modalContainer") {
+            createModal(sectionId, id, editables[i]);
+
+        } else if (id == "carousel") {
+
+            createCarousel(sectionId, id, editables[i]);
+
+        } else {
+            if (jsonData[sectionId] != null) {
+                var section = jsonData[sectionId]
+
+                if (section[id] != null) {
+                    if (editables[i].tagName == "IMG") {
+                        editables[i].src = "../" + section[id];
+                    } else {
+                        editables[i].innerHTML = section[id];
+                    }
+
+                }
+            }
+        }
+
+    }
+}
+
+function createCarousel(sectionId, id, editable) {
+    var carouselSection = document.getElementById("carousel-generic");
+
+    // console.log(editable.firstElementChild.innerHTML);
+    // console.log(jsonData[sectionId].images)
+    var dots = "";
+    var imageDivs = "";
+    var images = jsonData[sectionId].images;
+
+    for (var i = 0; i < images.length; i++) {
+        if (i == 0) {
+            dots += `<li data-target="#carousel-generic" data-slide-to="0" class="active"></li>`;
+            imageDivs += `
+            <div class="item active">
+                <img class="fill" src="` + images[i] + `" alt="...">
+                <div class="carousel-caption">
+                    <!-- <h3>Caption Text</h3> -->
+                </div>
+            </div>
+            `
+        } else {
+            dots += `<li data-target="#carousel-generic" data-slide-to="` + i + `"></li>`;
+            imageDivs += `
+            <div class="item">
+                <img class="fill" src="` + images[i] + `" alt="...">
+                <div class="carousel-caption">
+                    <!-- <h3>Caption Text</h3> -->
+                </div>
+            </div>
+            `
+        }
+    }
+
+    editable.firstElementChild.innerHTML = `
+    <ol class="carousel-indicators">
+        ` + dots + `
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+        ` + imageDivs + `
+    </div>
+
+    <!-- Controls -->
+    <a class="left carousel-control" href="#carousel-generic" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+    </a>
+    <a class="right carousel-control" href="#carousel-generic" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+    </a>
+    `;
+
+}
+
+function createModal(sectionId, id, editable) {
+
+    // editable.innerHTML = "";
+
+    var modalSection = document.getElementById("modalSection");
+    // console.log(modalSection.innerHTML);
+
+
+
+    var columns = "col-md-12 col-sm-12";
+    if (jsonData[sectionId].columns == "3") {
+        columns = "col-md-4 col-sm-6";
+    } else if (jsonData[sectionId].columns == "4") {
+        columns = "col-md-3 col-sm-6";
+    } else if (jsonData[sectionId].columns == "2") {
+        columns = "col-md-6 col-sm-6";
+    } else if (jsonData[sectionId].columns == "5") {
+        columns = "col-md-2 col-sm-6";
+    } else if (jsonData[sectionId].columns == "1") {
+        columns = "col-md-12 col-sm-12";
+    }
+
+    for (var i = 0; i < jsonData[sectionId].modals.length; i++) {
+        modal = jsonData[sectionId].modals[i]
+
+        console.log();
+        var link;
+        var image = modal.image;
+        var title = modal.title;
+
+
+
+        var price = modal.price;
+        var extraText = modal.extraText;
+        var element;
+
+        var items = "";
+        var link = "portfolio_" + sectionId + "_" + i;
+
+        if (jsonData[sectionId].noLink == "false") {
+        for (var j = 0; j < modal.items.length; j++) {
+            if (isArray(modal.items[j])) {
+
+                subModal = modal.items[j];
+                items += "<ul>";
+                for (var k = 0; k < subModal.length; k++) {
+                    items += "<li>" + subModal[k] + "</li>";
+                }
+                items += "</ul>";
+            } else {
+                items += "<li>" + modal.items[j] + "</li>\n";
+            }
+        }
+
+            var element = `<div class=\"` + columns + ` portfolio-item">
+                <a href="#` + link + `" class="portfolio-link" data-toggle="modal">
+                    <div class="portfolio-hover">
+                        <div class="portfolio-hover-content">
+                            <i class="fa fa-search-plus fa-3x"></i>
+                        </div>
+                    </div>
+                    <img src="` + image + `" class="img-responsive" alt="">
+                    <div class="portfolio-caption">
+                        <h4>` + title + `</h4>
+                        <p class="text-muted">` + price + `</p>
+                    </div>
+                </a>
+            </div>
+            `
+            var modalElement = `<div class="portfolio-modal modal fade" id="` + link + `" tabindex="-1" role="dialog" aria-hidden="true">
+                                  <div class="modal-dialog">
+                                      <div class="modal-content">
+                                          <div class="close-modal" data-dismiss="modal">
+                                              <div class="lr">
+                                                  <div class="rl">
+                                                  </div>
+                                              </div>
+                                          </div>
+                                          <div class="container">
+                                              <div class="row">
+                                                  <div class=".col-lg-1 col-lg-offset-2">
+                                                      <div class="modal-body">
+                                                          <h2>` + title + `</h2>
+                                                          <p class="item-intro text-muted">` + price + "</br>" + extraText + `</p>
+                                                          <ul>
+                                                          ` + items + `
+                                                          </ul>
+
+                                                          <img class="img-responsive img-rounded" src="` + image + `" alt="">
+                                                          <button type="button" class="btn btn-primary" data-dismiss="modal"><i class="fa fa-times"></i> Luk</button>
+                                                      </div>
+                                                  </div>
+                                              </div>
+                                          </div>
+                                      </div>
+                                  </div>
+                              </div>`;
+            modalSection.innerHTML += modalElement;
+        } else {
+            var element = `<div class=\"` + columns + ` portfolio-item">
+                    <img src="` + image + `" class="img-responsive" alt="">
+                    <div class="portfolio-caption">
+                        <h4>` + title + `</h4>
+                        <p class="text-muted">` + price + `</p>
+                    </div>
+            </div>
+            `
+        }
+        // for (jj = 0; jj < modal.items)
+        editable.innerHTML += element;
+    }
+}
+
+function getSection(id) {
+    return id.split(" ")[0]
+}
+
+function getId(id) {
+    return id.split(" ")[1]
+}
+
+function isArray(what) {
+    return Object.prototype.toString.call(what) === '[object Array]';
+}
